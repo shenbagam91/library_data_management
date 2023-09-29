408ddaf21505fc72afd7d3efeebaa3633bad10ca
Shenbagam L <127710519+shenbagam91@users.noreply.github.com>
2023-09-29 20:15:53 +0530

    Update book-router.js

--- routes/books/book-router.js
+++ routes/books/book-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const Book = require('../../models/book');
 
 const { bookRegister,bookGet,bookUpdate,bookRemove } = require('../../controller/book-router');
 
@@ -7,5 +8,15 @@ router.get('/book/get',bookGet);
 router.put('/book/update/:id',bookUpdate);
 router.delete('/book/remove/:id',bookRemove);
 
+router.get('/getbook/:id',async(req,res)=>{
+    const id = req.params.id;
+    try{
+        await Book.findById({_id:id})
+        .then(users => res.json(users))
+        .catch(err => console.log(err));
+    }catch(err){
+        console.log(err);
+    }
+});
 
 module.exports = router;
